ef8f0877391af03f8504119fb1baa1a4cc142587
henrique moura <h3dema@users.noreply.github.com>
2025-04-03 23:26:04 +0200

    Comparison

--- tasks.py
+++ tasks.py
@@ -5,6 +5,11 @@ class Task:
         self.name = name
         self.priority = priority
         self.burst_time = burst_time
+        
+    def __lt__(self, other):
+        # Higher priority tasks will come first in the priority queue
+        return self.priority > other.priority
+
 
 def create_queues(tasks, priority_ranges):
     queues = [deque() for _ in range(len(priority_ranges))]
