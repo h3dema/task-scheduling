15942c0c52f09cb84dc1bfc839470275da5480c8
h3dema <henriquemoura@hotmail.com>
2025-04-04 12:23:10 +0200

    refactored

---
+++ utils.py
@@ -0,0 +1,18 @@
+
+
+def can_run(task, completed_tasks):
+    """
+    Determines if a task can be executed based on its dependencies.
+
+    A task is allowed to run if all the tasks it depends on have been completed.
+
+    Parameters:
+        task (TaskSrv2): The task to be checked.
+        completed_tasks (set): A set containing the names of completed tasks.
+
+    Returns:
+        bool: True if all dependencies of the task are met, False otherwise.
+    """
+
+    # Check if all dependencies of the task are satisfied
+    return all(dep in completed_tasks for dep in task.dependencies)
