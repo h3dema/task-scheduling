0cb689cb9339afc3633a6193e0759687d33c6ecc
h3dema <henriquemoura@hotmail.com>
2025-04-04 12:23:31 +0200

    can_run in utils.py

--- svr2_mlfq_with_dependencies.py
+++ svr2_mlfq_with_dependencies.py
@@ -25,24 +25,8 @@ import heapq
 
 from svr2_mlfq import TaskSrv2, aging
 from tasks import create_priority_queues
+from utils import can_run
 
-
-def can_run(task, completed_tasks):
-    """
-    Determines if a task can be executed based on its dependencies.
-
-    A task is allowed to run if all the tasks it depends on have been completed.
-
-    Parameters:
-        task (TaskSrv2): The task to be checked.
-        completed_tasks (set): A set containing the names of completed tasks.
-
-    Returns:
-        bool: True if all dependencies of the task are met, False otherwise.
-    """
-
-    # Check if all dependencies of the task are satisfied
-    return all(dep in completed_tasks for dep in task.dependencies)
 
 
 def svr2_mlfq_with_dependencies(queues, queue_quanta, task_quantum, aging_threshold, aging_increment):
